2fe20dcebaa348aa6721ad6e96325e71130b8557
Dawn <1540680885@qq.com>
2019-12-05 18:50:54 +0800

    test by dawn

---
+++ Pro19-spring-cloud-zuul-9002/src/main/java/com/atguigu/cloud/SpringCloudZuulApp.java
@@ -0,0 +1,14 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+public class SpringCloudZuulApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudZuulApp.class, args);
+        //testgit
+    }
+}
+
